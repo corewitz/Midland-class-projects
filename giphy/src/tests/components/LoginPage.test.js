3c25ad0ede756cc7703095e7359fcf480d34352f
corewitz <corewitz@gmail.com>
2023-05-10 17:46:28 -0500

    added second test

--- giphy/src/tests/components/LoginPage.test.js
+++ giphy/src/tests/components/LoginPage.test.js
@@ -6,43 +6,76 @@
 
 //! Mock a failure, and display a message to the user
 
-import React from "react";
-import LoginPage from "../../components/LoginPage";
-import { UserProvider } from "../../context/UserContext";
-import { render, fireEvent, screen } from "@testing-library/react";
-//import "@testing-library/jest-dom";
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { useUserContext } from '../../context/UserContext';
+import LoginPage from '../../components/LoginPage';
 
-describe('LoginPage', () => {
-    
-    let mockUser;
-    let mockSetUser;
-    let mockClearUser;
+jest.mock('../../context/UserContext', () => ({
+    useUserContext: jest.fn(),
+}));
 
+describe('Login Page', () => {
     beforeEach(() => {
-        mockUser = null;
-        mockSetUser = jest.fn();
-        mockClearUser = jest.fn();
-    })
+        // Set default return values before each test
+        useUserContext.mockReturnValue({
+            user: null,
+            setUser: jest.fn(),
+            clearUser: jest.fn(),
+        });
+    });
 
-    it('should disable the button if username or password is less than 4 characters', () => {
-      const { getByTestId } = render(
-      <UserProvider 
-      value={{user: mockUser, setUser: mockSetUser, clearUser: mockClearUser}}>
-         <LoginPage />
-      </UserProvider>
-    );
 
-      const button = getByTestId ('button');
+    it('should render login page', () => {
+        // render the component directly without UserProvider since we're mocking it ourselves
+        const { getByPlaceholderText, getByTestId } = render(
+            <LoginPage />
+        );
 
-      expect(button).toBeDisabled();
+        const usernameInput = getByPlaceholderText('Username');
+        const passwordInput = getByPlaceholderText('Password');
+        const loginButton = getByTestId('button');
 
-      fireEvent.change(screen.getByPlaceholderText('Username'), {
-        target: {value: "Test"},
-      });
+        // initially button should be disabled
+        expect(loginButton).toBeDisabled();
 
-      fireEvent.change(screen.getByPlaceholderText('Password'), {
-        target: {value: "pa$$word"}
-      });
+        // enter less than 4 characters
+        fireEvent.change(usernameInput, { target: { value: 'abc' } });
+        fireEvent.change(passwordInput, { target: { value: '123' } });
+        
+        // button should still be disabled
+        expect(loginButton).toBeDisabled();
 
+        // enter 4 characters
+        fireEvent.change(usernameInput, { target: { value: 'abcd' } });
+        fireEvent.change(passwordInput, { target: { value: '1234' } });
+
+        // button should now be enabled
+        expect(loginButton).not.toBeDisabled();
+    });
+
+    it('should set username when login button is clicked', () => {
+        const setUser = jest.fn();
+        const mockContextValue = { setUser, user: null, clearUser: jest.fn() };
+
+        // Set the return value of the useUserContext hook
+        useUserContext.mockReturnValue(mockContextValue);
+
+        const { getByPlaceholderText, getByTestId } = render(
+            <LoginPage />
+        );
+
+        const usernameInput = getByPlaceholderText('Username');
+        const passwordInput = getByPlaceholderText('Password');
+        const loginButton = getByTestId('button');
+
+        fireEvent.change(usernameInput, { target: { value: 'abcd' } });
+        fireEvent.change(passwordInput, { target: { value: '1234' } });
+
+        fireEvent.click(loginButton);
+
+        expect(setUser).toHaveBeenCalledWith({ username: 'abcd' });
     });
 });
+
